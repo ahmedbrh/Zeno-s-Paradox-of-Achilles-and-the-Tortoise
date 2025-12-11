be5cbf9fcac85d047fcf9a23025a6937ef6a4da2
ahmedbrh <brhiliahmed@gmail.com>
2025-12-11 02:11:37 +0100

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Zeno-s-Paradox-of-Achilles-and-the-Tortoise/',
   server: {
     port: 3000,
     open: true
